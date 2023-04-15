8394d75f065e301a60db9ff806da8193f18bf0be
kelani34 <kelanitaiwo34@gmail.com>
2023-04-15 14:34:50 +0100

    feat/ sidebar active func

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,37 @@
 import React from "react";
 import Sidebar from "./Sidebar";
 
+const routes = [
+  {
+    path: "/",
+    name: "Dashboard",
+    icon: "/Dashboard.svg",
+  },
+  {
+    path: "/documents",
+    name: "Documents",
+    icon: "/Document.svg",
+  },
+  {
+    path: "/payments",
+    name: "Payments",
+    icon: "/Payment.svg",
+  },
+  {
+    path: "/calendar",
+    name: "Calendar",
+    icon: "/Calendar.svg",
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    icon: "/Profile.svg",
+  },
+];
 const Main = () => {
   return (
     <div className=" p-8">
-      <Sidebar />
+      <Sidebar routes={routes} />
     </div>
   );
 };
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,58 +1,38 @@
 import Image from "next/image";
-import React from "react";
+import React, { ReactNode } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-const Sidebar = () => {
+interface Props {
+  routes: { path: string; name: string; icon: string }[];
+}
+
+const Sidebar = ({ routes }: Props) => {
+  const { pathname } = useRouter();
+
   return (
-    <div className="bg-[#272D35] py-10 rounded-2xl">
-      <ul>
-        <li className=" my-6 flex">
-          <div className=" rounded-r-3xl w-2 h-8 bg-gradient-radial" />
-          <div className="flex items-center w-full gap-6 px-7 ">
-            <Image src={"/Home.svg"} width={15} height={15} alt="dashboard" />
-            <p className="text-white font-bold text-2xl">Dashboard</p>
-          </div>
-        </li>
-        <li className=" flex my-6">
-          <div className=" rounded-r-3xl w-2 h-8 bg-gradient-radial" />
-          <div className="flex items-center w-full gap-6 px-7">
-            <Image
-              src={"/Document.svg"}
-              width={15}
-              height={15}
-              alt="documents"
+    <div className="bg-[#272D35] py-10 rounded-2xl w-max">
+      <div>
+        {routes.map((route) => (
+          <Link className=" my-6 flex" key={route.path} href={route.path}>
+            <div
+              className={` rounded-r-3xl w-2 h-8 bg-gradient-radial ${
+                route.path === pathname ? "bg-gradient-radial" : "bg-none"
+              }`}
             />
-            <p className="text-[#C4C4C4] font-bold text-2xl text-left">
-              Documents
-            </p>
-          </div>
-        </li>
-        <li className=" flex my-6">
-          <div className=" rounded-r-3xl w-2 h-8 bg-gradient-radial" />
-          <div className="flex items-center w-full gap-6 px-7">
-            <Image src={"/Payment.svg"} width={15} height={15} alt="payments" />
-            <span className="text-[#C4C4C4] font-bold text-2xl">Payments</span>
-          </div>
-        </li>
-        <li className=" flex my-6">
-          <div className=" rounded-r-3xl w-2 h-8 bg-gradient-radial" />
-          <div className="flex items-center w-full gap-6 px-7">
-            <Image
-              src={"/Calendar.svg"}
-              width={15}
-              height={15}
-              alt="calender"
-            />
-            <span className="text-[#C4C4C4] font-bold text-2xl">Calendar</span>
-          </div>
-        </li>
-        <li className=" flex my-6">
-          <div className=" rounded-r-3xl w-2 h-8 bg-gradient-radial" />
-          <div className="flex items-center w-full gap-6 px-7">
-            <Image src={"/Profile.svg"} width={15} height={15} alt="profile" />
-            <span className="text-[#C4C4C4] font-bold text-2xl">Profile</span>
-          </div>
-        </li>
-      </ul>
+            <div className="flex items-center w-full gap-6 px-7 ">
+              <Image src={route.icon} width={15} height={15} alt={route.name} />
+              <p
+                className={`${
+                  route.path === pathname ? "text-white" : "text-[#C4C4C4]"
+                } font-medium text-2xl`}
+              >
+                {route.name}
+              </p>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
