6778d74c2981b1112f8d72c6a5ec17332b9b7533
kelani34 <kelanitaiwo34@gmail.com>
2023-04-15 15:22:35 +0100

    cleanup: sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -16,11 +16,11 @@ const Sidebar = ({ routes }: Props) => {
   };
 
   return (
-    <div className="bg-[#272D35] py-10 rounded-2xl w-max">
+    <div className="bg-[#272D35] py-1 rounded-2xl w-max">
       <div className="flex flex-col">
         {routes.map((route) => (
           <Link
-            className=" my-6 flex style-link  "
+            className=" my-6 flex style-link mr-8  "
             key={route.path}
             href={route.path}
           >
@@ -52,7 +52,8 @@ const Sidebar = ({ routes }: Props) => {
         <div className="border border-[#C4C4C4] opacity-50 m-10" />
       </div>
       <div className="flex flex-col">
-        <div className="flex items-center w-full gap-6 my-6">
+        <div className="flex my-6 cursor-pointer">
+          <div className={`w-2 `} />
           <div
             className={`flex items-center w-full gap-6 px-7 ${
               isLightMode ? "opacity-100" : "opacity-50"
@@ -76,10 +77,12 @@ const Sidebar = ({ routes }: Props) => {
           </div>
         </div>
 
-        <Link
-          href={"/settings"}
-          className=" flex items-center w-full gap-6 my-6"
-        >
+        <Link href={"/settings"} className=" flex  my-6">
+          <div
+            className={`style-bg-gradient rounded-r-3xl w-2 h-8 bg-gradient-radial  ${
+              "settings" === pathname ? "bg-gradient-radial" : "bg-none"
+            }`}
+          />
           <div className="flex items-center w-full gap-6 opacity-50 px-7">
             <Image
               src={"/settings.svg"}
@@ -93,7 +96,8 @@ const Sidebar = ({ routes }: Props) => {
             </span>
           </div>
         </Link>
-        <Link href={"/login"} className=" flex items-center w-full gap-6 my-6">
+        <Link href={"/login"} className=" flex my-6">
+          <div className={`w-2 `} />
           <div className="flex items-center w-full gap-6  px-7">
             <Image
               src={"/logout.svg"}
