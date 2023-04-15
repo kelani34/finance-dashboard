f5b6639917c001b2105589c0b115901bc9cf3610
kelani34 <kelanitaiwo34@gmail.com>
2023-04-15 13:30:56 +0100

    add: light mode and dark mode tailwind setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [darkMode, setDarkMode] = useState(false);
+  useEffect(() => {
+    const isDarkMode = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    setDarkMode(isDarkMode);
+  }, []);
+  return (
+    <div
+      className={`bg-${darkMode ? "gray-900" : "white"} text-${
+        darkMode ? "white" : "gray-900"
+      }`}
+    >
+      <Component {...pageProps} />
+    </div>
+  );
 }
